ea3108c6cba9102401d2e3ee6fc537b9990fa54a
Yonas44 <tesfu.yonas1350@gmail.com>
2023-01-31 19:57:50 +0300

    Decorate classes using decorate pattern

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :parent_permission
 
-  def initialize(name, age, parent_permission)
+  def initialize(name, age, parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -23,5 +26,32 @@ class Person
     return false unless parent_permission
 
     true
+  end
+
+  def correct_name
+    @name
+  end
+end
+
+class BaseDecorator < Nameable
+  def initialize(props)
+    super()
+    @nameable = props
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name[0, 10]
   end
 end
